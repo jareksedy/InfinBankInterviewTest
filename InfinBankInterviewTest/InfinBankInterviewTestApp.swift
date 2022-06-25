860766dbfd7c2c1e335b031c67ff0f2b3095900b
Jarek Šedý <jareksedy@icloud.com>
2022-06-25 08:56:09 +0600

    Initial Commit

---
+++ InfinBankInterviewTest/InfinBankInterviewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinBankInterviewTestApp.swift
+//  InfinBankInterviewTest
+//
+//  Created by Ярослав on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct InfinBankInterviewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
